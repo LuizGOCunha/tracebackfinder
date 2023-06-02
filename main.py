8e1c8ed55c57548993f46fbcad1305e95cc87739
Luiz <valuiz1@gmail.com>
2023-06-02 08:33:07 -0300

    Organized logic into a function

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import re
+from typing import List
 
-with open('console.txt', "r") as file:
-    logs = file.read()
+def find_tracebacks(log:str) -> List[str]:
+    """
+    Looks for the patterns of Tracebacks inside a log string, 
+    then returns everything it finds.
+    """
+    lines_log = logs.split("\n")
 
-lines_log = logs.split("\n")
+    pattern1 = re.compile(r'\[.*\]:\s*Traceback \(most recent call last\):')
+    pattern2 = re.compile(r'\[.*\]:\s*File "/.*')
+    pattern3 = re.compile(r'(\[.*\]:\s*.*(Exception|Error): .*)')
 
-result1 = re.findall(r'\[.*\]:\s*Traceback \(most recent call last\):', logs)
-result2 = re.findall(r'\[.*\]:\s*File "/.*\n.*', logs)
-result3 = re.findall(r'(\[.*\]:\s*.*(Exception|Error): .*)', logs)
-print(len(result1))
-print(len(result2))
-print(len(result3))
+    traceback_string = pattern1.match
+    file_tracing_string = pattern2.match
+    exception_string = pattern3.match
 
-pattern1 = re.compile(r'\[.*\]:\s*Traceback \(most recent call last\):')
-pattern2 = re.compile(r'\[.*\]:\s*File "/.*')
-pattern3 = re.compile(r'(\[.*\]:\s*.*(Exception|Error): .*)')
+    traceback_list = []
+    for index, line in enumerate(lines_log):
+            
+        if traceback_string(line):
+            # create line
+            string = line + "\n"
 
-traceback_string = pattern1.match
-file_tracing_string = pattern2.match
-exception_string = pattern3.match
+        elif file_tracing_string(line):
+            # Add the file tracing + the next line
+            string += (line + "\n")
+            string += (lines_log[index+1] + "\n")
 
-traceback_list = []
-for index, line in enumerate(lines_log):
+        elif exception_string(line):
+            # Add the exception and adds to list (final line)
+            string += (line + "\n")
+            traceback_list.append(string)
+    return traceback_list
+
+if __name__ == "__main__":
+    # Get logs
+    with open('console.txt', "r") as file:
+        logs = file.read()
         
-    if traceback_string(line):
-        # create line
-        string = line + "\n"
-
-    elif file_tracing_string(line):
-        # Add the file tracing + the next line
-        string += (line + "\n")
-        string += (lines_log[index+1] + "\n")
-
-    elif exception_string(line):
-        # Add the exception and adds to list (final line)
-        string += (line + "\n")
-        traceback_list.append(string)
-
-for x in traceback_list:
-    print(x)
+    # Process logs
+    traceback_list = find_tracebacks(logs)
+    print(len(traceback_list))
+    for x in traceback_list:
+        print(x)
 
